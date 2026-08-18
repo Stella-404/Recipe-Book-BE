3bd62a44946e17ae1e3bfe17c748765876a79ab0
Haleema Waheed <haleema.waheed@fiveriverstech.local>
2026-08-18 12:30:53 +0500

    Display Image from DB folder on webpage

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 # importing the router file, tat were seperated to organize the code
 from Routers import endpoints
+from fastapi.staticfiles import StaticFiles  
 
 
 app = FastAPI()
 # including the router files
 app.include_router(endpoints.router)
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 #  Enable CORS so REACT can communicate with FASTAPI
 app.add_middleware(
